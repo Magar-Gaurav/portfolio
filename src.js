18adb0f415c6552dcb1bf399a5a2b3ddbdf12bab
Magar-Gaurav <gauravmagar664@gmail.com>
2025-03-17 16:26:12 +0545

    add js

---
+++ src.js
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+      var typed =new Typed(".auto-type",{
+        strings: ["Gaurav Magar"],
+        typeSpeed :150,
+        backSpeed :150,
+        loop :true
+      });
