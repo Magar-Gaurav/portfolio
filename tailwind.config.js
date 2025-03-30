9900856780c106d21e0de0611efba38769b6766b
Magar-Gaurav <gauravmagar664@gmail.com>
2025-03-30 21:58:40 +0545

    add new portfolio page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          gloria: ['Gloria Hallelujah', 'cursive'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
